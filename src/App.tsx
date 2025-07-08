6b68f9826be8de89b0ec5f50f7f97715197c5b08
Ingo Steinke <ingosteinke@gmx.de>
2025-07-08 18:36:20 +0200

    improve responsive output

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import MyLineChart from './components/MyLineChart';
 import MyBarChart from './components/MyBarChart';
 import './App.css'
+import {ResponsiveContainer} from "recharts";
 
 function App() {
     return (
@@ -8,15 +9,20 @@ function App() {
             <header>
                 <h1>React & Recharts Data Dashboard</h1>
             </header>
-            <main className="dashboard">
-                <div className="chart-card">
-                    <h2>Monthly Revenue vs. Expenses</h2>
-                    <MyLineChart/>
-                </div>
-                <div className="chart-card">
-                    <h2>Monthly Profit</h2>
-                    <MyBarChart/>
-                </div>
+
+            <main className="dashboard dashboard--flex">
+                <ResponsiveContainer>
+                    <div className="chart-card">
+                        <h2>Monthly Revenue vs. Expenses</h2>
+                        <MyLineChart/>
+                    </div>
+                </ResponsiveContainer>
+                <ResponsiveContainer>
+                    <div className="chart-card">
+                        <h2>Monthly Profit</h2>
+                        <MyBarChart/>
+                    </div>
+                </ResponsiveContainer>
             </main>
         </div>
     );
